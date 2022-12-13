f4576b4a05b9e6a8a00a4a9a862eeaf57a9dec94
tomoki-music <i.tomoki0218@gmail.com>
2022-12-13 21:47:22 +0900

    ferture/slick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,33 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-import '@fortawesome/fontawesome-free/js/all';
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+$(document).on ("turbolinks:load", function(){
+    $('.slider').slick({
+      autoplay: true, //自動再生
+      infinite: true, //スライドのループ有効化
+      dots: true, //ドットのナビゲーションを表示
+      autoplaySpeed: 4000, //再生スピード
+      slidesToShow: 1, //表示するスライドの数
+      slidesToScroll: 1, //スクロールで切り替わるスライドの数
+      prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+      nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+      responsive: [{
+        breakpoint: 768, //ブレークポイントが768px
+        settings: {
+          slidesToShow: 1, //表示するスライドの数
+          slidesToScroll: 1, //スクロールで切り替わるスライドの数
+        }
+      }, {
+        breakpoint: 480, //ブレークポイントが480px
+        settings: {
+          slidesToShow: 1, //表示するスライドの数
+          slidesToScroll: 1, //スクロールで切り替わるスライドの数
+        }
+      }]
+    });
+  });
