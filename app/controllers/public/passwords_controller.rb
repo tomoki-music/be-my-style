fd4bfc689691859f6a70bd8514b6ca4ac448cdc8
tomoki-music <i.tomoki0218@gmail.com>
2023-05-05 19:56:48 +0900

    fix/devise-password

--- app/controllers/public/passwords_controller.rb
+++ app/controllers/public/passwords_controller.rb
@@ -23,12 +23,12 @@ class Public::PasswordsController < Devise::PasswordsController
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    public_homes_top_path
+  end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    public_homes_top_path
+  end
 end
