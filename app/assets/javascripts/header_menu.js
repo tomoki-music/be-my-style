96ec09e2a8022e98d30dfaadad1f2bb58cd9737f
tomoki-music <i.tomoki0218@gmail.com>
2023-05-25 01:25:46 +0900

    feature/responsive-menu

---
+++ app/assets/javascripts/header_menu.js
@@ -0,0 +1,19 @@
+'use strict';
+
+window.onload = function(){
+  const open = document.getElementById('open');
+  const menu = document.querySelector('.customer-menu-sp');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    menu.classList.add('show');
+  });
+
+  close.addEventListener('click', () => {
+    menu.classList.remove('show');
+  });
+
+  menu.addEventListener('click', () => {
+    close.click();
+  });
+}
