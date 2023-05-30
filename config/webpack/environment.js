b6e6785cb8720414873fa5e476f337b23bc07c23
tomoki-music <i.tomoki0218@gmail.com>
2023-05-30 20:32:01 +0900

    modify/environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
-)
+const webpack = require('webpack');
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $:      require.resolve("jquery"),
+  jQuery: require.resolve("jquery")
+}))
 
 module.exports = environment
