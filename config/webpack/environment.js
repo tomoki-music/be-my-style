384cd5dcf528e98c0b4a7b9c124db77c5acd0716
tomoki-music <i.tomoki0218@gmail.com>
2023-05-30 21:24:10 +0900

    fix/yarn-add-jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack');
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $:      require.resolve("jquery"),
-  jQuery: require.resolve("jquery")
-}))
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
 
 module.exports = environment
