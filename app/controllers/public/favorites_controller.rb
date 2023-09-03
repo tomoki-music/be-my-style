1f5f4cc4466d6fcbcf01d287e43740e869d2f9c8
tomoki-music <i.tomoki0218@gmail.com>
2023-09-03 22:25:43 +0900

    fix/favorite-design

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,12 @@ class Public::FavoritesController < ApplicationController
   def create
     @activity_favorite = Favorite.new(customer_id: current_customer.id, activity_id: params[:activity_id])
     @activity_favorite.save
-    redirect_to public_activity_path(params[:activity_id])
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @activity_favorite = Favorite.find_by(customer_id: current_customer.id, activity_id: params[:activity_id])
     @activity_favorite.destroy
-    redirect_to public_activity_path(params[:activity_id])
-    
+    redirect_back(fallback_location: root_path)
   end
 end
