db8b1286c8a8289a2e035dc2897b38bd8d2b18b9
tomoki-music <i.tomoki0218@gmail.com>
2024-02-06 11:58:11 +0900

    fix/request-notification-kaminari

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,7 +1,7 @@
 class Public::NotificationsController < ApplicationController
   before_action :authenticate_customer!
   def index
-    @notifications = current_customer.passive_notifications
+    @notifications = current_customer.passive_notifications.page(params[:page]).per(10)
     @notifications.where(checked: false).each do |notification|
       notification.update_attribute(:checked, true)
     end
