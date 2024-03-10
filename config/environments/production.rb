8fc134d36b396db67e9c69195cf9f36fb0c5db5c
tomoki-music <i.tomoki0218@gmail.com>
2024-03-10 19:12:56 +0900

    fix/production-setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,11 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
