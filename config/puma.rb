e71dd7a701a9f3f3e130cb8abca2b4a9edd0cc98
tomoki-music <i.tomoki0218@gmail.com>
2025-05-11 19:02:52 +0900

    puma.rbの修正

--- config/puma.rb
+++ config/puma.rb
@@ -4,25 +4,25 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 10 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+# environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -40,17 +40,40 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
 
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-rails_root = Dir.pwd
-if Rails.env.production?
-  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
-  stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log'),
-    File.join(rails_root, 'log', 'puma-error.log'),
-    true
-  )
-  daemonize
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# rails_root = Dir.pwd
+# if Rails.env.production?
+#   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+#   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+#   stdout_redirect(
+#     File.join(rails_root, 'log', 'puma.log'),
+#     File.join(rails_root, 'log', 'puma-error.log'),
+#     true
+#   )
+#   daemonize
+# end
+
+# Puma configuration for Rails production
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 10 }
+threads threads_count, threads_count
+
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# Use UNIX socket for Nginx upstream
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+if ENV['RAILS_ENV'] == 'production'
+  stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+  pidfile "#{app_root}/tmp/pids/puma.pid"
+  state_path "#{app_root}/tmp/pids/puma.state"
+  daemonize true
 end
+
+plugin :tmp_restart
