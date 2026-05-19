f176ec6fbbbb109a14e5fd50342f059cea1f0c56
tomoki-music <i.tomoki0218@gmail.com>
2026-05-20 00:42:52 +0900

    feature/recap-movie-top-cta

--- spec/requests/singing/homes_spec.rb
+++ spec/requests/singing/homes_spec.rb
@@ -19,5 +19,48 @@ RSpec.describe "Singing::Homes", type: :request do
       expect(response.body).to include("歌唱・演奏診断プラン")
       expect(response.body).to include("診断履歴を見る")
     end
+
+    context "Recap Movie CTA（ログイン済み）" do
+      before { sign_in singing_customer }
+
+      it "Recap Movie がない場合に説明文を表示すること" do
+        get singing_root_path
+
+        expect(response.body).to include("診断を続けると、あなたの年間まとめ動画が作成されます")
+      end
+
+      it "completed な Recap Movie がある場合に視聴リンクを表示すること" do
+        FactoryBot.create(:singing_generated_recap_movie, :completed, customer: singing_customer)
+
+        get singing_root_path
+
+        expect(response.body).to include("今年のまとめ動画を見る")
+        expect(response.body).to include(singing_recap_movies_path)
+      end
+
+      it "processing な Recap Movie がある場合に生成中バッジを表示すること" do
+        FactoryBot.create(:singing_generated_recap_movie, :processing, customer: singing_customer)
+
+        get singing_root_path
+
+        expect(response.body).to include("生成中です")
+      end
+
+      it "failed な Recap Movie がある場合に失敗バッジを表示すること" do
+        FactoryBot.create(:singing_generated_recap_movie, :failed, customer: singing_customer)
+
+        get singing_root_path
+
+        expect(response.body).to include("生成に失敗しました")
+      end
+    end
+
+    context "未ログイン時" do
+      it "Recap Movie CTA を表示しないこと" do
+        get singing_root_path
+
+        expect(response.body).not_to include("Recap Movie")
+      end
+    end
   end
 end
